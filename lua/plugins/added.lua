ac46d5eff95324e24dbd00427399ef9e4637543c
mushfiq <mushfiqxrabbi@gmail.com>
2024-03-12 15:51:17 +0600

    ...

---
+++ lua/plugins/added.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+  },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    keys = {
+      {
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "ge",
+        "<cmd>lua require('spider').motion('ge')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        mode = { "n", "o", "x" },
+      },
+    },
+  },
+}
--- lua/plugins/modified.lua
+++ lua/plugins/modified.lua
@@ -3,6 +3,28 @@ return {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
+        sort_by = "insert_after_current",
+      },
+    },
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
         always_show_bufferline = true,
       },
     },
@@ -50,5 +72,13 @@ return {
         "vimdoc",
       }
     end,
+  },
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      manual_mode = false,
+      detection_methods = { "pattern", "lsp" },
+      patterns = { ".root", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+    },
   },
 }
