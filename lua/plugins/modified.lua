0c987d0d1746e2ec77e2739b64501588cd796195
mushfiq <mushfiqxrabbi@gmail.com>
2024-03-12 06:39:29 +0600

    initial commit

---
+++ lua/plugins/modified.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, {
+          focusable = false,
+        })
+      end,
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      indent = {
+        char = "▏",
+        tab_char = "▏",
+      },
+    },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      symbol = "▏",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = {
+        "c",
+        "lua",
+        "query",
+        "vim",
+        "vimdoc",
+      }
+    end,
+  },
+}
